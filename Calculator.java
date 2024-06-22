37e54b563edaecc36fb6e278d5d24e2f1d0cc552
kirito1987 <laudro.pineda1987@gmail.com>
2024-06-22 11:11:47 -0500

    New features in progress

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
@@ -16,5 +18,21 @@ public class Calculator {
             throw new IllegalArgumentException("Division by zero");
         }
         return a / b;
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Your Calculator!");
+        System.out.println("To start enter the first number:");
+
+        double num1 = scanner.nextDouble();
+
+        System.out.println("\nGreat! Your First number is " + num1 + "\n");
+        System.out.println("Now enter the next number: ");
+
+        double num2 = scanner.nextDouble();
+
+        System.out.println("\nGreat, your next number is: " + num2);
     }
 }
